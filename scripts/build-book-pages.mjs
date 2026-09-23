28a50d3489337c31fce3879333c1953004225042
SWFT Studios <elombe@swftstudios.com>
2026-09-23 18:57:48 -0400

    Stop iOS autofill from populating booking spam trap and dropping legitimate orders

--- scripts/build-book-pages.mjs
+++ scripts/build-book-pages.mjs
@@ -166,7 +166,7 @@ function renderTierPage(tier) {
               <p class="book-step-lead">A few details help us make your order feel like yours.</p>
             <div class="ga-hp" aria-hidden="true">
               <label for="swft_hp_confirm">Leave blank</label>
-              <input type="text" id="swft_hp_confirm" name="swft_hp_confirm" tabindex="-1" autocomplete="off" data-lpignore="true" data-1p-ignore="true" data-form-type="other" value="">
+              <input type="hidden" id="swft_hp_confirm" name="swft_hp_confirm" value="">
             </div>
             <div class="ga-field">
               <label for="book-name">Name <span class="req">*</span></label>
